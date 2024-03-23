556713dae456dde9699c2812e41105a8ba37538b
jbarretodev <jbarretodev6105@gmail.com>
2024-03-23 02:27:42 -0400

    fix: resolving bugs

--- src/components/inventory/InventoryForm.tsx
+++ src/components/inventory/InventoryForm.tsx
@@ -8,7 +8,7 @@ import toast from "react-hot-toast";
 const InventoryForm = () => {
   const [listProduct, setListProduct] = useState<Product[]>([]);
   const [operation, setOperation] = useState<OperationCreate>({
-    type_op: 0,
+    type_op: 1,
     quantity: 0,
     product_id: 0,
   });
@@ -59,55 +59,61 @@ const InventoryForm = () => {
     ProductRequest.getProducts().then((rs) => {
       if (rs.status === 200 && Array.isArray(rs.data.products)) {
         setListProduct(rs.data.products);
+        setOperation({...operation,product_id:rs.data.products[0].id})
       }
     });
   }, []);
 
   return (
     <>
-      <h3 className="text-3xl font-bold mb-4 text-center mt-10">
+      <h3 className='text-3xl font-bold mb-4 text-center mt-10'>
         Registro de Actividad de Inventario
       </h3>
-      <div className="flex items-center justify-center h-screen -mt-40">
-        <Card className="w-1/2">
+      <div className='flex items-center justify-center h-screen -mt-40'>
+        <Card className='w-1/2'>
           <form>
-            <div className="mb-4">
-              <Label htmlFor="product" value="Seleccione el producto" />
-              <Select
-                onChange={handleInputChange}
-                name="product_id"
-                id="product"
-              >
-                {listProduct.map((product: Product) => (
-                  <option key={product.code} value={product.id}>
-                    {product.name}
-                  </option>
-                ))}
-              </Select>
+            <div className='mb-4'>
+              <Label htmlFor='product_id' value='Seleccione el producto' />
+              {listProduct.length > 0 && (
+                <Select
+                  defaultValue={listProduct[0].id}
+                  onChange={handleInputChange}
+                  name='product_id'
+                  id='product_id'
+                >
+                  {listProduct.map((product: Product) => (
+                    <option key={product.code} value={product.id}>
+                      {product.name}
+                    </option>
+                  ))}
+                </Select>
+              )}
             </div>
 
-            <div className="mb-4">
-              <Label htmlFor="action" value="Seleccione la operacion" />
-              <Select onChange={handleInputChange} name="type_op" id="action">
+            <div className='mb-4'>
+              <Label htmlFor='action' value='Seleccione la operacion' />
+              <Select defaultValue={1} onChange={handleInputChange} name='type_op' id='action'>
                 <option value={1}>Agregar</option>
-                <option value={0}>Retirar</option>
+                <option value={0}>
+                  Retirar
+                </option>
               </Select>
             </div>
 
-            <div className="mb-4">
+            <div className='mb-4'>
               <Label
-                htmlFor="quantity"
-                value="Cantidad por unidad o por kilo"
+                htmlFor='quantity'
+                value='Cantidad por unidad o por kilo'
               />
               <TextInput
                 onChange={handleInputChange}
-                id="quantity"
-                name="quantity"
-                type="text"
-                sizing="md"
+                id='quantity'
+                name='quantity'
+                type='text'
+                sizing='md'
               />
             </div>
-            <Button onClick={sendOperation} className="w-full">
+            <Button onClick={sendOperation} className='w-full'>
               Registrar Operacion
             </Button>
           </form>
